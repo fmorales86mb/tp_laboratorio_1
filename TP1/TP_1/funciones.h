23edafefd0d202a9b15db6b576b7588c1c9311c5
SGCBA\fmorales <fmorales86mb@gmail.com>
2017-09-08 15:16:32 -0300

    Se agregan referencias en las cabeceras.

--- TP1/TP_1/funciones.h
+++ TP1/TP_1/funciones.h
@@ -1,16 +1,56 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
 
+/**
+* \brief Realiza la suma de dos variables float.
+* \param nro1 primer valor float.
+* \param nro2 segundo valor float.
+* \return Devuelve, en un tipo de dato float, el valor de la suma.
+*
+*/
 float sumarFloat (float nro1, float nro2);
 
+/**
+* \brief Realiza la resta de dos variables float.
+* \param nro1 primer valor float.
+* \param nro2 segundo valor float.
+* \return Devuelve, en un tipo de dato float, el valor de la resta.
+*
+*/
 float restarFloat (float nro1, float nro2);
 
+/**
+* \brief Realiza la multiplicación de dos variables float.
+* \param nro1 primer valor float.
+* \param nro2 segundo valor float.
+* \return Devuelve, en un tipo de dato float, el valor de la multiplicación.
+*
+*/
 float multiplicarFloat (float nro1, float nro2);
 
+/**
+* \brief Realiza la división de dos variables float.
+* \param nro1 primer valor float.
+* \param nro2 segundo valor float, no debe ser igual a cero.
+* \return Devuelve, en un tipo de dato float, el valor de la división.
+*
+*/
 float dividirFloat (float nro1, float nro2);
 
+/**
+* \brief Realiza el factorial de una variable int.
+* \param nro entero positivo.
+* \return Devuelve, en un tipo de dato int, el valor del factorial.
+*
+*/
 int factorial (int nro);
 
+/**
+* \brief Chequea que el nro ingresado sea entero.
+* \param nro variable float a ser chequeada.
+* \return Devuelve 1 si es entero, 0 si no lo es.
+*
+*/
 int chequearNroEntero (float nro);
 
 #endif // FUNCIONES_H_INCLUDED
